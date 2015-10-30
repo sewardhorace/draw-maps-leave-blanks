925ce4a821c5c7e527283eb876407a2ee0709049
Max White <maxjwhite22@gmail.com>
2015-10-29 21:41:19 -0400

    some styles

--- js/mappy.js
+++ js/mappy.js
@@ -217,9 +217,9 @@ init();
 
 function init() {
   var s = new CanvasState(document.getElementById('exampleCanvas'));
-  s.addShape(new Shape(40,40,50,50)); // The default is gray
-  s.addShape(new Shape(60,140,40,60, 'lightskyblue'));
-  // Lets make some partially transparent
-  s.addShape(new Shape(80,150,60,30, 'rgba(127, 255, 212, .5)'));
-  s.addShape(new Shape(125,80,30,80, 'rgba(245, 222, 179, .7)'));
+  // s.addShape(new Shape(40,40,50,50)); // The default is gray
+  // s.addShape(new Shape(60,140,40,60, 'lightskyblue'));
+  // // Lets make some partially transparent
+  // s.addShape(new Shape(80,150,60,30, 'rgba(127, 255, 212, .5)'));
+  // s.addShape(new Shape(125,80,30,80, 'rgba(245, 222, 179, .7)'));
 }
